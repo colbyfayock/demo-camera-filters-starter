05c04942d49eaa0b6a6bcafa339c6339c846ae3d
colbyfayock <fayock@gmail.com>
2022-03-08 15:05:04 -0500

    removing unneeded imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
-import { useState } from 'react';
 import Head from 'next/head';
-import Image from 'next/image';
 
 import Layout from '@components/Layout';
 import Container from '@components/Container';
 import Button from '@components/Button';
 
-import styles from '@styles/Home.module.scss'
+import styles from '@styles/Home.module.scss';
 
 export default function Home() {
   return (
